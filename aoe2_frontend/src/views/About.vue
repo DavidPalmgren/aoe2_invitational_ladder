c3c6e4074bae44d01ec111e1b9b6637c73095620
DavidPalmgren <swemultigamers@gmail.com>
2026-05-27 22:20:42 +0200

    about

--- aoe2_frontend/src/views/About.vue
+++ aoe2_frontend/src/views/About.vue
@@ -1,6 +1,7 @@
 <template>
 <div>
-  <h1>Stuff</h1>
+  <h1> No Stuff</h1>
+  <p>This is a simple about page. The only reason there's text here is to check that everything is being uploaded correctly.</p>
 </div>
 
 
